11fa7b3873b4dba83b5bb933cbda5dab9df96832
Nicholas Ho <nicholasho14@yahoo.com>
2015-09-29 21:00:40 -0700

    Added comment

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -33,7 +33,7 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
-
+//this is a comment
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
